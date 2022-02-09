67b2a853337be288f1c77b3086568fd2edb034ae
Virtus Tan <virtustanofficial@gmail.com>
2022-02-09 19:50:57 +0700

    Update Index

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Spacevid from './stockgallery/sky.mp4';
+import title from './stockgallery/title.png';
+import vrlogo from './stockgallery/vr.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Test <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div onContextMenu={e => e.preventDefault()}>
+      <video 
+        class="videobackgroundspace"
+        autoplay="true"
+        loop="true"
+        muted="true"
+      ><source src={Spacevid} type="video/mp4"/>
+      </video>
+      </div>
+      <div class="titlebar">
+        <div>
+        <img src={vrlogo} alt="logo" id="vrlogo"/>
+        <img src={title} alt="title" id="titlelogo"/>
+        </div>
+        <span>Discover the new world  you've never see</span>
+        <div class="circle"></div>
+      </div>
     </div>
   );
-}
+} 
 
 export default App;
