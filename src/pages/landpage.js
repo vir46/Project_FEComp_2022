cb993f62b18958783d49b1713ed9a34eb34cf5dc
Virtus Tan <50411627+virtustan@users.noreply.github.com>
2022-02-15 16:39:54 +0700

    Button Landing Page

--- src/pages/landpage.js
+++ src/pages/landpage.js
@@ -1,13 +1,34 @@
-import React from "react";
+import {
+    React,
+    useEffect
+} from "react";
 import styles from "./landpage.module.css";
+import titles from '../stockgallery/title.png';
+import { Button } from 'react-bootstrap';
 import '@lottiefiles/lottie-player';
 
 
 function Landingpage(){
+    
+    useEffect(() => {
+        var animateButton = function(e) {
+
+            e.preventDefault();
+            e.target.classList.remove('animate');
+            e.target.classList.add('animate');
+            setTimeout(function(){
+              e.target.classList.remove('animate');
+            },700);
+          };
+        //   var bubblyButtons = document.getElementById("bubblybutton");
+        //   bubblyButtons.addEventListener("click", animateButton, false);
+    });
     return(
         <div>
             {/* Head Title */}
             <div class={styles.headcontainer}>
+                <img src={titles} alt="title" id={styles.titlelogo}/>
+                <Button id={styles.bubblybutton}>hello</Button>
                 <div id={styles.lottievr}>
                     <lottie-player autoplay loop src="https://assets6.lottiefiles.com/packages/lf20_n0b0gzpq.json"></lottie-player>
                 </div>
@@ -15,5 +36,6 @@ function Landingpage(){
         </div>
     );
 }
+
 export default Landingpage;
 
