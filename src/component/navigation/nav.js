c32cf8fc428218945f92a254174af06e0a590b9f
Virtus Tan <50411627+virtustan@users.noreply.github.com>
2022-02-16 12:49:00 +0700

    Tailwind Convert

--- src/component/navigation/nav.js
+++ src/component/navigation/nav.js
@@ -7,8 +7,9 @@ function Navigasi(){
         <div class="navcont h-10">
             <nav class="absolute navMenu">
                 <a class="text-center text-orange-100 no-underline uppercase font-medium inline-block w-20" href="https://github.com">Home</a>
-                <a class="text-center text-orange-100 no-underline uppercase font-medium inline-block w-20" href="https://github.com">Blog</a>
-                <a class="text-center text-orange-100 no-underline uppercase font-medium inline-block w-20" href="https://github.com">Work</a>
+                <a class="text-center text-orange-100 no-underline uppercase font-medium inline-block w-20" href="https://github.com">How To?</a>
+                <a class="text-center text-orange-100 no-underline uppercase font-medium inline-block w-20" href="https://github.com">Login</a>
+                <a class="text-center text-orange-100 no-underline uppercase font-medium inline-block w-20" href="https://github.com">FAQ</a>
                 <a class="text-center text-orange-100 no-underline uppercase font-medium inline-block w-20" href="https://github.com">About</a>
                 <div class="w-2 h-2 bg-yellow-500 opacity-0 dot"></div>
             </nav>
