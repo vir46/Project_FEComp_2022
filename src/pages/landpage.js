84c102d065e773c13ab1567d1c7180c662e557bf
kevinghebre <kevinghebre20@gmail.com>
2022-02-17 16:16:54 +0700

    u

--- src/pages/landpage.js
+++ src/pages/landpage.js
@@ -4,7 +4,8 @@ import titles from '../stockgallery/title.png';
 import { Button } from 'react-bootstrap';
 import '@lottiefiles/lottie-player';
 import { FaRocket } from 'react-icons/fa';
-import computer from '../asset3d/plane/Airplane.glb';
+import plane from '../asset3d/plane/Airplane.glb';
+import Bumi from '../component/parallax/imageearth.js';
 
 const Landingpage = () => {
     return(
@@ -34,7 +35,36 @@ const Landingpage = () => {
                         Join us and become part of the future of virtual reality.
                     </p>
                 </div>
-                <model-viewer alt="Plane" src={computer} poster="shared-assets/models/NeilArmstrong.webp" seamless-poster camera-controls></model-viewer>
+                <model-viewer alt="Plane" src={plane} poster="shared-assets/models/NeilArmstrong.webp" seamless-poster camera-controls></model-viewer>
+            </div>
+            <div>
+                <Bumi/>
+            </div>
+            <div className={styles.content1}>
+                <div class={styles.container1}>
+                    <span className={[styles.blackfont + " " + styles.headingcontent1]}>What is Valverse?</span>
+                    <p className={[styles.blackfont + " " + styles.bodycontent1]}>
+                        Valverse is a platform that connects people with the new virtual world.
+                        Built by a team of passionate developers, Valverse connect billion of people together.
+                        What u cant do in the real world, u can do in the virtual world. 
+                        Embrace the power of infinite possibilities.
+                        Join us and become part of the future of virtual reality.
+                    </p>
+                </div>
+                <model-viewer alt="Plane" src={plane} poster="shared-assets/models/NeilArmstrong.webp" seamless-poster camera-controls></model-viewer>
+            </div>
+            <div className={styles.content1}>
+                <div class={styles.container1}>
+                    <span className={[styles.blackfont + " " + styles.headingcontent1]}>What is Valverse?</span>
+                    <p className={[styles.blackfont + " " + styles.bodycontent1]}>
+                        Valverse is a platform that connects people with the new virtual world.
+                        Built by a team of passionate developers, Valverse connect billion of people together.
+                        What u cant do in the real world, u can do in the virtual world. 
+                        Embrace the power of infinite possibilities.
+                        Join us and become part of the future of virtual reality.
+                    </p>
+                </div>
+                <model-viewer alt="Plane" src={plane} poster="shared-assets/models/NeilArmstrong.webp" seamless-poster camera-controls></model-viewer>
             </div>
         </div>
     );
